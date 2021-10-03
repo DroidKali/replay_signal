3fd2db3bc2d1017be6e067304e1cad42a98a1e13
DroidKali <DroidKali@users.noreply.github.com>
2021-10-03 23:21:25 +0800

    add rtl433_replay.py

---
+++ rtl433_replay.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Create by DroidKali
+import bitstring
+from rflib import *
+
+freq = input("\033[31m请输入频率：\033[0m")
+start = b'\x80\x00\x00\x00\x00'
+bits= input("\033[31m请输入RTL_433原始信号数据：\033[0m")
+bs = ''
+for s in bits:
+    if s == '1':
+        bs += '1110'
+    else:
+        bs += '1000'
+
+bytes = bitstring.BitArray(bin=bs).tobytes()
+out = start + bytes
+
+d = RfCat()
+d.setFreq(int(freq))
+d.setMdmModulation(MOD_ASK_OOK)
+d.setMdmDRate(2500)
+d.setMaxPower()
+#0=x88 1=xee f=x8e
+
+print("\033[31m要发射的信号数据为：\033[0m")
+print(out)
+print("\033[32m开始发送。。。\033[0m")
+d.RFxmit(out*15)
+print("\033[31m发送完成。\033[0m")
