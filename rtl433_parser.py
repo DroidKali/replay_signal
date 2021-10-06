47e2e607a668a479a2286f4b84d134fecbfa3892
DroidKali <DroidKali@users.noreply.github.com>
2021-10-06 14:33:32 +0800

    rtl433_parser.py: add banner and author

--- rtl433_parser.py
+++ rtl433_parser.py
@@ -4,9 +4,19 @@
 import bitstring
 import argparse
 from rflib import *
+banner='''
+\033[31m ____           _ _         _   _            _             \033[0m
+\033[32m|  _ \ __ _  __| (_) ___   | | | | __ _  ___| | _____ _ __ \033[0m
+\033[33m| |_) / _` |/ _` | |/ _ \  | |_| |/ _` |/ __| |/ / _ \ '__|\033[0m
+\033[34m|  _ < (_| | (_| | | (_) | |  _  | (_| | (__|   <  __/ |   \033[0m
+\033[35m|_| \_\__,_|\__,_|_|\___/  |_| |_|\__,_|\___|_|\_\___|_|   \033[0m
+'''
+print(banner)
+print("\033[32mauthor: \033[31m记忆里的纯真\033[0m")
+print("\033[34mversion: \033[35m1.0\n\033[0m")
 
-parser = argparse.ArgumentParser(description='A python script to sent replay signal.')
-parser.add_argument('-f', action="store", default="433920000", dest="baseFreq",help='Specify the target frequency to transmit on, default is 433920000.',type=int)
+parser = argparse.ArgumentParser(description='\033[34mA python script to sent replay signal.\033[33m')
+parser.add_argument('-f', action="store", default="433920000", dest="baseFreq",help='Specify the target frequency to transmit on, default is 433920000.',type=float)
 parser.add_argument('-b', action="store", dest="bitstring",help='Specify the bits from RTL_433 raw code.',type=str)
 parser.add_argument('-r', action="store", dest="baudRate", default="2500", help='Specify the baudrate of the signal, default is 2500.',type=int)
 parser.add_argument('-x', action="store", dest="multiply", default="15", help='Specify the multiply of the signal, default is 15.',type=int)
